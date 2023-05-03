99f0bfa5b28b98425c4b4780d69bd8c2b51e8457
kumarmanishrai <manishkumarrai79@gmail.com>
2023-05-03 11:32:33 +0530

    completed chat app

---
+++ client/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import "./App.css";
+
+import io from "socket.io-client";
+import Chat from "./Chat";
+
+const socket = io.connect("http://localhost:5000");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true)
+    }
+  };
+
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            name="username"
+            placeholder="Manish..."
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="text"
+            name="room"
+            placeholder="Room Id..."
+            onChange={(e) => setRoom(e.target.value)}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const app = express()
+const http = require("http")
+const cors = require("cors")
+const {Server} = require("socket.io")
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["POST", "GET"],
+    }
+})
+
+io.on("connection", (socket)=> {
+    console.log(`User connected ${socket.id}`);
+
+    socket.on("join_room", (data)=> {
+        socket.join(data)
+        console.log(`user with ID = ${socket.id} joined room: ${data}`);
+    })
+
+    socket.on("send_message", (data)=> {
+        console.log(data);
+        socket.to(data.room).emit("recieve_message", data)
+    })
+
+    socket.on("disconnect", ()=> {
+        console.log("User disconnected", socket.id);
+    })
+})
+
+
+server.listen(5000, ()=> {
+    console.log("SERVER IS RUNNING ON PORT 5000");
+})
