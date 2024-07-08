768ce65fc729d84746c4be05d4d6338121f01b30
Manish Kumar Rai <98271764+kumarmanishrai@users.noreply.github.com>
2024-07-08 22:42:05 +0530

    Update index.js origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://mern-chat-phi.vercel.app",
         methods: ["POST", "GET"],
     }
 })
