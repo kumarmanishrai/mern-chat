382d720cffee8a2a81064619b74f83b2313752b8
Manish Kumar Rai <98271764+kumarmanishrai@users.noreply.github.com>
2024-07-09 01:28:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,14 @@ const {Server} = require("socket.io")
 
 app.use(cors())
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'none'; img-src 'self' https://mern-chat-ddv1.vercel.app");
+  res.setHeader("Content-Security-Policy", `
+    default-src 'self' 'unsafe-inline' https://mern-chat-ddv1.vercel.app https://mern-chat-phi.vercel.app;
+    img-src 'self' data: https://mern-chat-ddv1.vercel.app https://mern-chat-phi.vercel.app;
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mern-chat-ddv1.vercel.app https://mern-chat-phi.vercel.app;
+    style-src 'self' 'unsafe-inline' https://mern-chat-ddv1.vercel.app https://mern-chat-phi.vercel.app;
+    connect-src 'self' https://mern-chat-ddv1.vercel.app https://mern-chat-phi.vercel.app ws://localhost:3000 ws://mern-chat-ddv1.vercel.app ws://mern-chat-phi.vercel.app;
+    frame-src 'self' https://mern-chat-ddv1.vercel.app https://mern-chat-phi.vercel.app;
+  `);
   next();
 });
 
